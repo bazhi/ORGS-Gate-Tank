d65466626d072ab9bd206f50d3be337beb4ceba4
bazhicc <bazhicc@163.com>
2018-06-29 19:41:24 +0800

    no message

--- setLimit_mac.sh
+++ setLimit_mac.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+OS_TYPE=$(getOsType)
+if [ $OS_TYPE == "MACOS" ]; then
+	sudo sysctl -w kern.maxfiles=1048600
+	sudo sysctl -w kern.maxfilesperproc=1048576
+	sudo sysctl -w kern.ipc.somaxconn=4096
+	ulimit -n 1048576
+	ulimit -a
 
-sudo sysctl -w kern.maxfiles=1048600
-sudo sysctl -w kern.maxfilesperproc=1048576
-sudo sysctl -w kern.ipc.somaxconn=4096
-ulimit -n 1048576
-ulimit -a
+	sudo sysctl net.inet.tcp.keepintvl=1000
+	sudo sysctl net.inet.tcp.keepidle=1000
+	sudo sysctl net.inet.tcp.keepcnt=5
+else
+	ulimit -n 1048576
+	ulimit -a
+fi
 
-sudo sysctl net.inet.tcp.keepintvl=1000
-sudo sysctl net.inet.tcp.keepidle=1000
-sudo sysctl net.inet.tcp.keepcnt=5
 
