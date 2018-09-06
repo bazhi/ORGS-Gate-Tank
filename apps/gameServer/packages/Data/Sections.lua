42e5696ceb0cbb9fd6f1da074a102a000e510e06
bazhicc <bazhicc@163.com>
2018-09-06 15:38:46 +0800

    no message

--- apps/gameServer/packages/Data/Sections.lua
+++ apps/gameServer/packages/Data/Sections.lua
@@ -15,7 +15,7 @@ function Sections:getChapterStar(chapter_cid)
     for _, v in ipairs(values) do
         if v._data.chapter_cid == chapter_cid then
             star = star + v._data.star
-            if v.star > 0 then
+            if v._data.star > 0 then
                 count = count + 1
             end
         end
