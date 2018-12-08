5930028cde707c0b5d76835ee2c7df1cabeae4c9
bazhicc <bazhicc@163.com>
2018-12-08 10:51:44 +0800

    no message

--- src/packages/gbc/NginxBootstrap.lua
+++ src/packages/gbc/NginxBootstrap.lua
@@ -1,17 +1,17 @@
 --[[
-
+ 
 Copyright (c) 2015 gameboxcloud.com
-
+ 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
+ 
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
+ 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-
+ 
 ]]
 
 local Factory = cc.import(".Factory")
@@ -36,7 +36,7 @@ function NginxBootstrap:runapp(appRootPath)
     if type(upgrade) == "table" then
         upgrade = upgrade[1]
     end
-
+    cc.dump(headers)
     local classNamePrefix = "HttpInstance"
     local appConfig = self._configs[appRootPath]
     if upgrade and string.lower(upgrade) == "websocket" then
@@ -49,7 +49,7 @@ function NginxBootstrap:runapp(appRootPath)
             ngx.exit(ngx.HTTP_FORBIDDEN)
         end
     end
-
+    
     local connect = Factory.create(appConfig, classNamePrefix)
     connect:run()
 end
